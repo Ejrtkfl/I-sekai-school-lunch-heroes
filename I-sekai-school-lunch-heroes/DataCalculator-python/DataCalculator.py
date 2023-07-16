565627b51779130e0c8d078499040e19151577ac
Hello <132136050+THISISDAEKYUNG@users.noreply.github.com>
2023-07-17 00:26:41 +0900

    Update DataCalculator.py

--- I-sekai-school-lunch-heroes/DataCalculator-python/DataCalculator.py
+++ I-sekai-school-lunch-heroes/DataCalculator-python/DataCalculator.py
@@ -5,11 +5,10 @@ plt.rc('font', size = 12)
 plt.rc('axes', unicode_minus = False)
 plt.rcParams['font.family'] = 'Malgun Gothic'
 
-
-fig = plt.figure(figsize = (10, 5))
+fig = plt.figure(figsize = (16, 9))
 ax = fig.add_subplot(1, 1, 1)
 
-with open('C:\\test.json', 'r') as f:
+with open('C:\\Users\\ilove\\OneDrive\\바탕 화면\\test.json', 'r') as f:
 
     json_data = json.load(f)
 
@@ -19,9 +18,9 @@ color_list=list(range(len(json_data)))
 
 for i in range(len(json_data)):
     a=json_data[i]
-    b=str(a["variant"])
+    b=a["variant"]
     c=a["time"]
-    variant_list[i]=b+'%'
+    variant_list[i]=str(round(b*100,2))
     time_list[i]=c
 
 for i in range(len(json_data)):
@@ -30,8 +29,8 @@ for i in range(len(json_data)):
     else:
         color_list[i]='blue'
 
-ax.set_xlabel('변이율', fontsize=16)
-ax.set_ylabel('걸린 시간(초)', fontsize=16)
+ax.set_xlabel('변이율(단위: %)', fontsize=16)
+ax.set_ylabel('걸린 시간', fontsize=16)
 ax.set_title('변이율에 따른 알고리즘 성능 평가', fontsize=20, loc='left')
 
 plt.bar(variant_list, time_list, color = color_list, width = 0.3)
